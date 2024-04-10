97a9b3c949db81987afdcbb0804a8df91a1c7ad7
Nitesh Agarwal <niteshkumar.nk649@gmail.com>
2024-04-10 22:08:17 +0530

    component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { appleImg, bagImg, searchImg } from "../utils";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <img src={appleImg} alt="apple" width={14} height={18} />
+        <div>
+          {["Phones", "Macbooks", "Tablets"].map((nav) => (
+            <div key={nav}>{nav}</div>
+          ))}
+          <div>
+            <img src={searchImg} alt="search" width={18} height={18} />
+            <img src={bagImg} alt="bag" width={18} height={18} />
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
