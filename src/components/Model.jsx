fd4a5e29ba8771c416e3c9706234ed95d941b348
Nitesh Agarwal <niteshkumar.nk649@gmail.com>
2024-04-15 10:11:23 +0530

    setup modelview

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,8 +1,33 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import React from "react";
+import React, { useRef, useState } from "react";
+import ModelView from "./ModelView";
+import { yellowImg } from "../utils";
+
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
 
 const Model = () => {
+  const [size, setSize] = useState("small");
+  const [model, setModel] = useState({
+    title: "iPhone 15 Pro in Natural Titanium",
+    color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
+    img: yellowImg,
+  });
+
+  //   camera control for the model view
+  const cameraControlSmall = useRef();
+  const cameraControlLarge = useRef();
+
+  //   model
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
+
+  //   rotation
+  const [smallRotation, setSmallRotation] = useState(0);
+  const [largeRotation, setLargeRotation] = useState(0);
+
   useGSAP(() => {
     gsap.to("#heading", {
       y: 0,
@@ -17,9 +42,46 @@ const Model = () => {
           Take a closer look.
         </h1>
         <div className="flex flex-col items-center mt-5">
-            <div className=" w-full h-[75vh] md:h-[90vh] overflow-hidden relative ">
-                
-            </div>
+          <div className=" w-full h-[75vh] md:h-[90vh] overflow-hidden relative ">
+            <ModelView
+              index={1}
+              groupRef={small}
+              gsapType="view1"
+              controlRef={cameraControlSmall}
+              setRotationState={setSmallRotation}
+              item={model}
+              size={size}
+            />
+            <ModelView
+              index={2}
+              groupRef={large}
+              gsapType="view2"
+              controlRef={cameraControlLarge}
+              setRotationState={setLargeRotation}
+              item={model}
+              size={size}
+            />
+
+            <Canvas
+              className=" w-full h-full"
+              style={{
+                position: "fixed",
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                overflow: "hidden",
+              }}
+              eventSource={document.getElementById("root")}
+            >
+              <View.Port />
+            </Canvas>
+          </div>
+          <div className=" mx-auto w-full">
+            <p className=" text-sm font-light text-center mb-5">
+              {model.title}
+            </p>
+          </div>
         </div>
       </div>
     </section>
